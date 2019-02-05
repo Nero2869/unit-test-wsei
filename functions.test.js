9aff1eb48c9abba30b06a5218a8e6a4d8fc45b19
Szymon <szymon@MacBook-Air-Szymon.local>
2019-02-05 18:04:26 +0100

    Usunieto komentarze i uporzadkowano kod

--- functions.test.js
+++ functions.test.js
@@ -1,15 +1,5 @@
 const functions = require('./functions');
 
-// Working with async data
-
-// Promise
-// test('User fetched name should be Leanne Graham', () => {
-//   expect.assertions(1);
-//   return functions.fetchUser().then(data => {
-//     expect(data.name).toEqual('Leanne Graham');
-//   });
-// });
-
 // Async Await
 test('User fetched name should be Leanne Graham', async () => {
   expect.assertions(1);
--- libraryPage.test.js
+++ libraryPage.test.js
@@ -1,12 +1,10 @@
 let webdriver = require('selenium-webdriver'),
-    { after, before} = require('selenium-webdriver/testing'),
     By = webdriver.By,
     until = webdriver.until,
     element,
     element2,
     element3,
     actual,
-    expected,
     driver;
 
 const waitUntilTime = 20000;
@@ -79,45 +77,4 @@ describe('Testy FireAppBase', () => {
     });
 
 });
-
-// it('Uruchom driver', () => {
-//     return d.then(_d => {
-//         driver = _d
-//     })
-// });
-//
-// it('Zainicjalizuj kontekst strony', async () => {
-//     await driver.get(rootURL);
-// });
-
-
-// //szukamy naszego inputa
-// driver.findElement(By.css('#ember14'))
-//     .sendKeys('user@testwsei.pl')
-//     .then(() => {
-//         console.log("Poprawnie wpisano dane do inputa")
-//     })
-//     .catch(() => {
-//         console.log("Wystąpił problem z wpisaniem danych do inputa");
-//     });
-//
-// //szukam guzika do zatwierdzania, guzik nie ma id więc biorę "copy selector" z konsoli
-// driver.findElement(By.css('#ember3 > div > div > div > div.col-xs-10.col-xs-offset-1.col-sm-offset-0.col-sm-4.col-md-3 > button'))
-//     .click()
-//     .then(() => {
-//         console.log("Pobrano guzik");
-//     })
-//     .catch(() => {
-//         console.log("Wystąpił problem z pobraniem guzika");
-//     });
-//
-// driver.wait(until.elementLocated(By.css('#ember3 > div > div > div.alert.alert-success')), 6500)
-//     .getText()
-//     .then((txt) => {
-//         console.log("Udało się poprawnie wysłac zapytanie do api. api zwróciło następujący respone w alercie " + txt);
-//     })
-//     .catch((error) => {
-//         console.log("Nie udało się pobrac elementu alert-success ponieważ: " + error);
-//     });
-
 
